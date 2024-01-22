e2fd105b61731539e1fd799b3b058abfbd48bb61
Dev Shaq <nwezecharles27@gmail>
2024-01-22 11:49:39 +0100

    first commit

---
+++ counter.js
@@ -0,0 +1,13 @@
+let count= 0
+let countNumber=document.getElementById("count-number")
+function increment(){
+     count= count + 1
+countNumber.innerHTML= count
+console.log(count)
+}
+
+function save(){
+console.log(count)
+}
+
+
